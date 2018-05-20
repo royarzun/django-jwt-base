0309c74de3ecb67220c073e3d4a81e7ba8182d0b
Ricardo Oyarzun <royarzun@gmail.com>
2018-05-20 09:23:18 -0400

    chore: add postgres settings

--- config/settings.py
+++ config/settings.py
@@ -99,8 +99,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hotlist',
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_URL'),
+        'PORT': 5432
     }
 }
 
