fc654f4caebd8542c58b03c015f54dc3fda227e3
Ricardo Oyarzun <royarzun@gmail.com>
2018-05-20 19:00:00 -0400

    feat(users): add account mainainers views

--- users_profile/views.py
+++ users_profile/views.py
@@ -1,9 +1,51 @@
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render_to_response
+from django.contrib.auth.forms import PasswordChangeForm, AdminPasswordChangeForm
+from django.shortcuts import render_to_response, redirect, render
 from django.template import RequestContext
+
+from social_django.models import UserSocialAuth
 
 
 @login_required(redirect_field_name='next', login_url='login')
 def home(request):
     return render_to_response("users_profile/home.html", {},
                               context_instance=RequestContext(request))
+
+
+@login_required
+def settings(request):
+    user = request.user
+    try:
+        facebook_login = user.social_auth.get(provider='facebook')
+    except UserSocialAuth.DoesNotExist:
+        facebook_login = None
+
+    can_disconnect = (user.social_auth.count() > 1 or user.has_usable_password())
+
+    return render(request, 'registration/settings.html', {
+        'facebook_login': facebook_login,
+        'can_disconnect': can_disconnect
+    })
+
+
+@login_required
+def password(request):
+    if request.user.has_usable_password():
+        PasswordForm = PasswordChangeForm
+    else:
+        PasswordForm = AdminPasswordChangeForm
+
+    if request.method == 'POST':
+        form = PasswordForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordForm(request.user)
+    return render(request, 'registration/password.html', {'form': form})
